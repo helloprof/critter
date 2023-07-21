6f025c756e1ca9f1a623659d09264b8dbf7cfe1e
helloprof <suluxan.seneca@gmail.com>
2023-07-21 16:35:19 -0400

    storing data in arrays

---
+++ src/critters.js
@@ -0,0 +1,34 @@
+window.critters = [
+  {
+    id: 1,
+    name: "racz",
+    username: "raccoon2012313",
+    links: [
+      {
+        url: "https://instagram.com/raccoon",
+        name: "Instagram"
+      },
+      {
+        url: "https://youtube.com/raccoon",
+        name: "Youtube"
+      }
+    ],
+    profilePicture: ""
+  },
+  {
+    id: 2,
+    name: "opo",
+    username: "oppossum23432243",
+    links: [
+      {
+        url: "https://instagram.com/raccoon",
+        name: "Instagram"
+      },
+      {
+        url: "https://youtube.com/raccoon",
+        name: "Youtube"
+      }
+    ],
+    profilePicture: ""
+  }
+]
---
+++ src/tweets.js
@@ -0,0 +1,23 @@
+window.tweets = [
+  {
+    id: 1,
+    critterID: 1,
+    date: "",
+    likes: 1,
+    replies: 3,
+    retweets: 10,
+    views: 1019231,
+    tweet: "trash day!",
+    imageURL: "https://images.unsplash.com/photo-1622559650168-61fa8ee44bf4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80" 
+  },
+  {
+    id: 2,
+    critterID: 1,
+    date: "",
+    likes: 1,
+    replies: 3,
+    retweets: 10,
+    views: 1019231,
+    tweet: "trash day is every tuesday??!",
+    imageURL: "https://images.unsplash.com/photo-1622559650168-61fa8ee44bf4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80" 
+  },]
