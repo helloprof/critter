08352fa74e40a28c36d5f85a80cc5fd49a15835b
helloprof <suluxan.seneca@gmail.com>
2023-08-04 15:41:12 -0400

    displaying all tweets and adding a submit event -  resetting dom on submit, resetting form, adding values from the form into the data array to re-display all tweets

--- src/app.js
+++ src/app.js
@@ -23,10 +23,33 @@ function displayTweets(critterID) {
     var mainPage = document.querySelector("#mainPage")
     tweets.forEach((tweet) => {
         if (critterID == tweet.id ) {
-            var card = document.createElement("card")
+            var card = document.createElement("div")
+
+            var cardHeader = document.createElement("div")
+            var image = document.createElement("img")
+
+            var name = document.createElement("h5")
+            var username = document.createElement("h6")
+            var tweetContent = document.createElement("p")
             // critterButton 
-            card.textContent = tweet.tweet 
+            // card.textContent = tweet.tweet 
+            name.textContent = tweet.name
+            username.textContent = tweet.username
+            tweetContent.textContent = tweet.tweet
+            image.src = tweet.imageURL
+
             card.className = "card"
+            cardHeader.className = "tweet"
+            image.className = "image"
+            
+            cardHeader.appendChild(name)
+            cardHeader.appendChild(username)
+            cardHeader.appendChild(tweetContent)
+
+            card.appendChild(cardHeader)
+            card.appendChild(image)
+
+
             // var cardImage = card.createElement("img")
             // cardImage.classname
             mainPage.appendChild(card)
@@ -34,9 +57,92 @@ function displayTweets(critterID) {
     })
 }
 
+
+
+// function displayAllTweets() {
+//     tweets.forEach((tweet) => {
+//         displayTweets(tweet.id)
+//     })
+// }
+
+function displayAllTweets(tweets) {
+    var mainPage = document.querySelector("#mainPage")
+    tweets.forEach((tweet) => {
+            var card = document.createElement("div")
+
+            var cardHeader = document.createElement("div")
+            var image = document.createElement("img")
+
+            var name = document.createElement("h5")
+            var username = document.createElement("h6")
+            var tweetContent = document.createElement("p")
+            // critterButton 
+            // card.textContent = tweet.tweet 
+            name.textContent = tweet.name
+            username.textContent = tweet.username
+            tweetContent.textContent = tweet.tweet
+            image.src = tweet.imageURL
+
+            card.className = "card"
+            cardHeader.className = "tweet"
+            image.className = "image"
+            
+            cardHeader.appendChild(name)
+            cardHeader.appendChild(username)
+            cardHeader.appendChild(tweetContent)
+
+            card.appendChild(cardHeader)
+            card.appendChild(image)
+
+
+            // var cardImage = card.createElement("img")
+            // cardImage.classname
+            mainPage.appendChild(card)
+    })
+}
+
+
+
+function resetTweetCards() {
+    var mainPage = document.querySelector("#mainPage") 
+    mainPage.innerHTML = ""
+}
+
+function addReview() {
+    var form = document.querySelector("#post") 
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        var date = new Date().toLocaleDateString
+
+        var name = document.querySelector("#name").value
+        var username = document.querySelector("#username").value
+        // console.log(username)
+        var imageURL = document.querySelector("#image").value
+        var tweet = document.querySelector("#postContent").value
+
+        var newTweet = {
+            name, 
+            username,
+            imageURL, 
+            tweet
+        }
+        
+        tweets.push(newTweet)
+        
+        console.log(tweets)
+        resetTweetCards()
+        form.reset()
+        displayAllTweets(tweets)
+
+    })
+}
+
 function onLoad() {
     console.log("loaded")
     generateTweetsButton()
+    displayAllTweets(tweets)
+    addReview()
 }
 
 window.addEventListener("load", onLoad)
